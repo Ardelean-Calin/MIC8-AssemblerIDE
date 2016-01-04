92eb0f79bace4bf696809263b82d8ddeab7ac114
Ardelean Călin Petru <acp.calin@gmail.com>
2016-01-04 17:51:13 +0200

    Added GUI.

---
+++ src/UI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by calin on 1/4/16.
+ */
+public class UI {
+    private JButton btnSimulate;
+    private JButton btnStep;
+    private JButton btnCompile;
+    private JButton btnExport;
+    private JEditorPane editor;
+    private JLabel lblStatus;
+    private JLabel lblA;
+    private JTable tblMemory;
+    private JTable tblStack;
+    private JLabel lblB;
+    private JLabel lblMbr;
+    private JLabel lblPc;
+}
